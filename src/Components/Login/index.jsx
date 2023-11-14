0870c08a7c256f03683b910f5be4cb2f45e6bdc5
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-11-14 00:29:48 -0300

    feat: Add sign in and up page and route

---
+++ src/Components/Login/index.jsx
@@ -0,0 +1,8 @@
+
+export function Login(){
+    return(
+        <div>
+            <p>Login</p>
+        </div>
+    )
+}
--- src/routes/authroutes.jsx
+++ src/routes/authroutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from '../components/Home'
+import { Home } from '../components/Home';
+import { Login } from '../components/Login';
 
 export function AuthRoutes(){
     return(
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     )
 }
