8b45f0111749b0fac945f0a525fc5b61a96091e8
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-11-14 00:30:00 -0300

    feat: Add css reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthRoutes } from './routes/authroutes';
+import { Header } from './Components/Header';
+import './App.css';
 
 function App() {
   return (
    <div>
     <BrowserRouter>
-      <Header />
+    <Header />
       <AuthRoutes />
       <Footer />
     </BrowserRouter>
